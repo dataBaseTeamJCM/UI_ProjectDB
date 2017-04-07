eb3ee1cb68402b5e4704d51d9c448f3498760736
Marwil Campos <marwilcampos@gmail.com>
2017-04-07 16:27:50 -0400

    Version b0.3

---
+++ src/model/connection/Conexion.java
@@ -0,0 +1,32 @@
+package model.connection;
+
+import java.sql.*;
+
+public class Conexion {
+
+	String user;
+	String url;
+	char[] password;
+	Connection connection = null;
+
+	public Connection Conectar()
+	{//
+			try{
+				System.out.println(url);
+				Class.forName("org.postgresql.Driver");
+				this.connection = DriverManager.getConnection
+								(url,this.user,
+								new String(this.password));
+			}catch(Exception e){
+				System.out.println("error");
+			}
+			return this.connection;
+	}
+	public Conexion(String usr, char[] password){
+		this.url ="jdbc:postgresql://localhost:5432/maraton";
+		System.out.println(this.url);
+		this.password =password;
+		this.user = usr;
+	}
+	
+}
---
+++ src/views/WindowPrincipal.java
@@ -0,0 +1,82 @@
+package views;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.border.EmptyBorder;
+
+import controller.Coordinator;
+
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class WindowPrincipal extends JFrame implements ActionListener{
+
+	private Coordinator myCoordinator;
+	private JTextArea txtpAreaInfo;
+	private JLabel lblDataComp;
+	private JButton btnInitSession;
+
+	/**
+	 * Launch the application.
+	 */
+	String textInfo="";
+
+	public void setCoordinator(Coordinator myCoordinator) {
+		this.myCoordinator = myCoordinator;
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public WindowPrincipal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		textInfo="Esta aplicacion presenta una herramienta para"
+				+ " la gestion de la base de datos del maraton de la competencia.\n\n"
+				+ "La aplicacion permite registrar,actualizar, buscar y eliminar registros"
+				+ "de la base de datos de la compentencia";
+		
+		txtpAreaInfo = new JTextArea();
+		txtpAreaInfo.setBounds(32, 63, 380, 140);
+		txtpAreaInfo.setEditable(false);
+		txtpAreaInfo.setFont(new java.awt.Font("Verdana", 0, 14));
+		txtpAreaInfo.setLineWrap(true);
+		txtpAreaInfo.setText(textInfo);
+		txtpAreaInfo.setWrapStyleWord(true);
+		txtpAreaInfo.setBorder(javax.swing.BorderFactory.createBevelBorder(
+				javax.swing.border.BevelBorder.LOWERED, null, null, null,
+				new java.awt.Color(0, 0, 0)));
+		
+		lblDataComp = new JLabel("BASE DE DATOS DE LA COMPETENCIA");
+		lblDataComp.setBounds(89, 31, 260, 15);
+		
+		btnInitSession = new JButton("Iniciar sesion");
+		btnInitSession.setBounds(142, 212, 140, 25);
+		
+		btnInitSession.addActionListener(this);
+		add(btnInitSession);
+		add(lblDataComp);
+		add(txtpAreaInfo);
+		
+		setSize(480, 350);
+		setTitle("BIENVENIDO");
+		setBounds(100, 100, 450, 300);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		getContentPane().setLayout(null);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource()==btnInitSession){
+			myCoordinator.showWindowLogin();
+		}
+	}
+}
