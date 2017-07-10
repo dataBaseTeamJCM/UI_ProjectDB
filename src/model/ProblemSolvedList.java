9307fd14a5a6459ad41b8d114fc9082a023a51a5
Marwil Campos <marwilcampos@gmail.com>
2017-07-10 11:16:46 -0400

    modificacion de la clase problema resuelto

--- src/model/ProblemSolvedList.java
+++ src/model/ProblemSolvedList.java
@@ -1,5 +1,32 @@
 package model;
 
-public class ProblemSolvedList {
+import java.util.ArrayList;
+
+public class ProblemSolvedList extends ArrayList<ProblemSolved> implements ICustomList{
+
+	private ProblemSolved instance;
+	
+	public ProblemSolvedList() {
+		// TODO Auto-generated constructor stub
+	}
+		
+	public ProblemSolved getInstance() {
+		return instance;
+	}
+
+	public void setInstance(ProblemSolved instance) {
+		this.instance = instance;
+	}
+
+	
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		String print = ""; 
+		for (ProblemSolved problemSolved : this) {
+			print += problemSolved.toString() + "\n";
+		}
+		System.out.println(print);
+	}
 
 }
