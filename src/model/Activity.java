7390753743255e02f4cd424a52175cefe16d6e1e
Marwil Campos <marwilcampos@gmail.com>
2017-07-10 12:32:25 -0400

    se implemente la clase activitity

---
+++ src/model/Activity.java
@@ -0,0 +1,78 @@
+package model;
+
+public class Activity {
+
+	private String id;
+	private String idCompetencia;
+	private String name;
+	private String place;
+	private String date;
+	
+	public Activity() {
+		super();
+		// TODO Auto-generated constructor stub
+		this.id	 			= "none";
+		this.idCompetencia 	= "none";
+		this.name			= "none";
+		this.place			= "none";
+		this.date			= "none";
+	}
+
+	public Activity(String id, String idCompetencia, String name, String place, String date) {
+		super();
+		this.id = id;
+		this.idCompetencia = idCompetencia;
+		this.name = name;
+		this.place = place;
+		this.date = date;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getIdCompetencia() {
+		return idCompetencia;
+	}
+
+	public void setIdCompetencia(String idCompetencia) {
+		this.idCompetencia = idCompetencia;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "Activity [id=" + id + ", idCompetencia=" + idCompetencia 
+				+ ", name=" + name + ", place=" + place
+				+ ", date=" + date + "]";
+	}
+	
+	
+}
