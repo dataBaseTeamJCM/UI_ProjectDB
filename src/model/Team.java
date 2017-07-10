42c6ebb94068320131efcbf18446b321760078f3
Marwil Campos <marwilcampos@gmail.com>
2017-07-10 00:22:29 -0400

    se implemento la clase problema

--- src/model/Team.java
+++ src/model/Team.java
@@ -1,5 +1,16 @@
 package model;
 
-public class Team {
+import java.util.ArrayList;
 
+public class Team {
+	private String 					id;				// identificador unico del equipo		
+	private String 					name;			// nombre del equipo
+	private String 					university;		// universidad a la que pertenece el equipo
+	private String 					school;			// escuela o facultad 
+	private int						year;			// año del equipo ej: 2005
+	private ArrayList<Professor> 	professors;		// lista de profesores del equipo (coach y tecnico)
+	private ArrayList<Student> 		students;		// lista de estudiantes del equipo 		
+	private ArrayList<Problem> 		problemsSolved;	// lista de problemas solucionados
+	
+	
 }
