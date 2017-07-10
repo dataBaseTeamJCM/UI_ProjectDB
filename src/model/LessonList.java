9beabfee08faa937b98f549597d25c973129adb1
Marwil Campos <marwilcampos@gmail.com>
2017-07-10 14:04:28 -0400

    se implemento la clase lista de lecciones

---
+++ src/model/LessonList.java
@@ -0,0 +1,36 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class LessonList extends ArrayList<Lesson> implements ICustomList {
+
+	private LessonList instance;
+	
+	public LessonList() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public LessonList getInstance() {
+		return instance;
+	}
+
+
+	public void setInstance(LessonList instance) {
+		this.instance = instance;
+	}
+
+
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		String print = "";
+		for (Lesson lesson : this) {
+			print += lesson.toString() + "\n";
+		}
+		System.out.println(print);
+	}
+
+}
