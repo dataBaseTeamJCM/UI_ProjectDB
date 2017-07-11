0223ee0871b44ebfa9f7313782a1e3d067c94979
Marwil Campos <marwilcampos@gmail.com>
2017-07-11 14:48:22 -0400

    implementacion de la clase action constants

---
+++ src/controller/ActionConstants.java
@@ -0,0 +1,13 @@
+package controller;
+
+public final class ActionConstants {
+
+	public static final String KEY_UP 		= "up";
+	public static final String KEY_DOWN 	= "down";
+	public static final String KEY_DELETE 	= "delete";
+	public static final String KEY_EDIT 	= "edit";
+	public static final String KEY_INSERT 	= "insert";
+	public static final String KEY_EXIT 	= "exit";
+
+	
+}
