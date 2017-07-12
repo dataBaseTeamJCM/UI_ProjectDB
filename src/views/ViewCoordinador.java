1daf440b73b0ffffbf3aafd92d1cef9586b8a8a3
Marwil Campos <marwilcampos@gmail.com>
2017-07-11 22:47:44 -0400

    modificacion del view coordinador

--- src/views/ViewCoordinador.java
+++ src/views/ViewCoordinador.java
@@ -22,6 +22,11 @@ import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 import java.awt.FlowLayout;
 import javax.swing.BoxLayout;
+import javax.swing.JTabbedPane;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
+import javax.swing.JTable;
+import javax.swing.JTree;
 
 public class ViewCoordinador extends JFrame implements ActionListener, IViews{
 
@@ -33,6 +38,9 @@ public class ViewCoordinador extends JFrame implements ActionListener, IViews{
 	private JButton btnInsert;
 	private JButton btnDelete;
 	private JButton btnExit;
+	private JPanel panelList;
+	private JScrollPane scrollPaneJtree;
+	private JTree tree;
 
 	public void setCoordinator(Coordinator myCoordinator) {
 		this.myCoordinator = myCoordinator;
@@ -40,7 +48,7 @@ public class ViewCoordinador extends JFrame implements ActionListener, IViews{
 
 	/**
 	 * Create the frame.
-	 */
+	 **/
 	public ViewCoordinador(Coordinator myCoordinator) {
 		this.myCoordinator = myCoordinator;
 		
@@ -53,7 +61,7 @@ public class ViewCoordinador extends JFrame implements ActionListener, IViews{
 		setContentPane(contentPane);
 		
 		addPanelButtons();
-		
+		addPanelList();
 		
 		
 		setVisible(true);
@@ -61,13 +69,92 @@ public class ViewCoordinador extends JFrame implements ActionListener, IViews{
 	}
 	
 	
+	public Coordinator getMyCoordinator() {
+		return myCoordinator;
+	}
+
+	public void setMyCoordinator(Coordinator myCoordinator) {
+		this.myCoordinator = myCoordinator;
+	}
+
+	public JButton getBtnUp() {
+		return btnUp;
+	}
+
+	public void setBtnUp(JButton btnUp) {
+		this.btnUp = btnUp;
+	}
+
+	public JButton getBtnDown() {
+		return btnDown;
+	}
+
+	public void setBtnDown(JButton btnDown) {
+		this.btnDown = btnDown;
+	}
+
+	public JButton getBtnEdit() {
+		return btnEdit;
+	}
+
+	public void setBtnEdit(JButton btnEdit) {
+		this.btnEdit = btnEdit;
+	}
+
+	public JButton getBtnInsert() {
+		return btnInsert;
+	}
+
+	public void setBtnInsert(JButton btnInsert) {
+		this.btnInsert = btnInsert;
+	}
+
+	public JButton getBtnDelete() {
+		return btnDelete;
+	}
+
+	public void setBtnDelete(JButton btnDelete) {
+		this.btnDelete = btnDelete;
+	}
+
+	public JButton getBtnExit() {
+		return btnExit;
+	}
+
+	public void setBtnExit(JButton btnExit) {
+		this.btnExit = btnExit;
+	}
+
+	public JPanel getPanelList() {
+		return panelList;
+	}
+
+	public void setPanelList(JPanel panelList) {
+		this.panelList = panelList;
+	}
+
+	public JScrollPane getScrollPaneJtree() {
+		return scrollPaneJtree;
+	}
+
+	public void setScrollPaneJtree(JScrollPane scrollPane) {
+		this.scrollPaneJtree = scrollPane;
+	}
+
+	public JTree getTree() {
+		return tree;
+	}
+
+	public void setTree(JTree tree) {
+		this.tree = tree;
+	}
+
 	@Override
 	public void addPanelButtons() {
 		// TODO Auto-generated method stub
 		JPanel panelButtons = new JPanel();
-		contentPane.add(panelButtons, BorderLayout.SOUTH);
-		panelButtons.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
+		panelButtons.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		btnUp = new JButton("<");
 		panelButtons.add(btnUp);
 		
@@ -99,8 +186,24 @@ public class ViewCoordinador extends JFrame implements ActionListener, IViews{
 		btnEdit.setActionCommand(ActionConstants.KEY_EDIT);
 		btnInsert.setActionCommand(ActionConstants.KEY_INSERT);
 		btnExit.setActionCommand(ActionConstants.KEY_EXIT);
+		contentPane.add(panelButtons, BorderLayout.SOUTH);
+		
 	}
 	
+	@Override
+	public void addPanelList() {
+		// TODO Auto-generated method stub
+		panelList = new JPanel();
+		contentPane.add(panelList, BorderLayout.WEST);
+		panelList.setLayout(new BoxLayout(panelList, BoxLayout.Y_AXIS));
+		
+		scrollPaneJtree = new JScrollPane();
+		panelList.add(scrollPaneJtree);
+		
+		tree = new JTree();
+		scrollPaneJtree.setViewportView(tree);
+		
+	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
@@ -128,5 +231,7 @@ public class ViewCoordinador extends JFrame implements ActionListener, IViews{
 				break;
 		}
 	}
+
+	
 
 }
