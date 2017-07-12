44d5879977889c0081e2d06d965f3e73f8a6b411
Marwil Campos <marwilcampos@gmail.com>
2017-07-11 22:47:05 -0400

    se implemento la clase competicion list

---
+++ src/model/CompetitionList.java
@@ -0,0 +1,44 @@
+package model;
+
+import java.util.ArrayList;
+
+import javax.swing.DefaultListModel;
+
+public class CompetitionList extends ArrayList<Competition> implements ICustomList {
+
+	private CompetitionList instance;
+	
+	
+	
+	
+	public CompetitionList() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CompetitionList getInstance() {
+		return instance;
+	}
+
+	public void setInstance(CompetitionList instance) {
+		this.instance = instance;
+	}
+
+	
+	@Override
+	public void print() {
+		// TODO Auto-generated method stub
+		String print = "";
+		for (Competition competition: this) {
+			print += competition.toString() + "\n";
+		}
+		System.out.println(print);
+	}
+
+	@Override
+	public DefaultListModel<String> toListModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
